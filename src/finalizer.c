2dbe09ee7bafc565eaa56e5b1ae2f37273c7d063
datwaft <datwaftx@gmail.com>
2023-04-20 18:30:26 -0600

    feat: implement initial creator and finalizer

--- src/finalizer.c
+++ src/finalizer.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "logging.h"
+#include "shared_memory.h"
+
+extern int errno;
 
 int main(int argc, char *argv[]) {
-  log_info("I am a finalizer.");
+  if (argc < 2) {
+    fprintf(stderr,
+            "Usage: %s <buffer name>"
+            "\n",
+            argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  const char *buffer_name = argv[1];
+
+  int err_free = free_shared_memory(buffer_name);
+  if (err_free == IPC_FAILURE) {
+    log_error("Error while freeing shared memory: "
+              "\x1b[1m"
+              "%s",
+              strerror(errno));
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
