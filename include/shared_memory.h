eca90fdc12d5de00a7fc737776400550dfaf0439
datwaft <datwaftx@gmail.com>
2023-04-20 18:02:41 -0600

    feat: add initial version of shared_memory library

---
+++ include/shared_memory.h
@@ -0,0 +1,17 @@
+#ifndef SHARED_MEMORY_H
+#define SHARED_MEMORY_H
+
+#define IPC_SUCCESS 0
+#define IPC_FAILURE -1
+
+typedef struct shared_mem_st {
+  int array[5];
+} shared_mem_t;
+
+shared_mem_t *create_shared_memory(const char *name);
+
+shared_mem_t *get_shared_memory(const char *name);
+
+int free_shared_memory(const char *name);
+
+#endif // !SHARED_MEMORY_H
---
+++ src/shared_memory.c
@@ -0,0 +1,49 @@
+#include "shared_memory.h"
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+shared_mem_t *create_shared_memory(const char *name) {
+  int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL);
+  if (fd == -1) {
+    return (void *)IPC_FAILURE;
+  }
+  int err = ftruncate(fd, sizeof(shared_mem_t));
+  if (err == -1) {
+    return (void *)IPC_FAILURE;
+  }
+  void *shm_addr = mmap(NULL, sizeof(shared_mem_t), PROT_READ | PROT_WRITE,
+                        MAP_SHARED, fd, 0);
+  if (shm_addr == MAP_FAILED) {
+    return (void *)IPC_FAILURE;
+  }
+  return shm_addr;
+}
+
+shared_mem_t *get_shared_memory(const char *name) {
+  int fd = shm_open(name, O_RDWR);
+  if (fd == -1) {
+    return (void *)IPC_FAILURE;
+  }
+  int err = ftruncate(fd, sizeof(shared_mem_t));
+  if (err == -1) {
+    return (void *)IPC_FAILURE;
+  }
+  void *shm_addr = mmap(NULL, sizeof(shared_mem_t), PROT_READ | PROT_WRITE,
+                        MAP_SHARED, fd, 0);
+  if (shm_addr == MAP_FAILED) {
+    return (void *)IPC_FAILURE;
+  }
+  return shm_addr;
+}
+
+int free_shared_memory(const char *name) {
+  int err = shm_unlink(name);
+  if (err == -1)
+    return IPC_FAILURE;
+  return IPC_SUCCESS;
+}
