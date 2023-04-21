0fc47ca760b554c0ed963326919068c83a5aaa49
datwaft <datwaftx@gmail.com>
2023-04-20 18:40:09 -0600

    feat: implement initial consumer and producer

--- src/consumer.c
+++ src/consumer.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "logging.h"
+#include "shared_memory.h"
+
+extern int errno;
 
 int main(int argc, char *argv[]) {
-  log_info("I am a consumer.");
+  if (argc < 2) {
+    fprintf(stderr,
+            "Usage: %s <buffer name>"
+            "\n",
+            argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  const char *buffer_name = argv[1];
+
+  shared_mem_t *shared_memory = get_shared_memory(buffer_name);
+  if (shared_memory == (void *)IPC_FAILURE) {
+    log_error("Error while obtaining shared memory: "
+              "\x1b[1m"
+              "%s",
+              strerror(errno));
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
