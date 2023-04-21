014ac071dd44297c6b07ff178bd9def20a221bac
datwaft <datwaftx@gmail.com>
2023-04-20 18:39:28 -0600

    fix(shared_memory): fix permissions and truncation on get

--- src/shared_memory.c
+++ src/shared_memory.c
@@ -8,7 +8,7 @@
 #include <unistd.h>
 
 shared_mem_t *create_shared_memory(const char *name) {
-  int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL);
+  int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
   if (fd == -1) {
     return (void *)IPC_FAILURE;
   }
@@ -27,10 +27,6 @@ shared_mem_t *create_shared_memory(const char *name) {
 shared_mem_t *get_shared_memory(const char *name) {
   int fd = shm_open(name, O_RDWR);
   if (fd == -1) {
-    return (void *)IPC_FAILURE;
-  }
-  int err = ftruncate(fd, sizeof(shared_mem_t));
-  if (err == -1) {
     return (void *)IPC_FAILURE;
   }
   void *shm_addr = mmap(NULL, sizeof(shared_mem_t), PROT_READ | PROT_WRITE,
