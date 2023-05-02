45a414813fb71dc7ba16e9d0c4fd2568b12dcaff
datwaft <datwaftx@gmail.com>
2023-05-02 14:33:26 -0600

    feat(producer): better message production log

--- src/producer.c
+++ src/producer.c
@@ -88,6 +88,7 @@ int main(int argc, char *argv[]) {
     }
 
     message_t message = message_new(id);
+    size_t field = shared_memory->circbuf.head;
     bool is_success = circbuf_atomic_push(&shared_memory->circbuf, message);
     if (!is_success) {
       log_warn("Something went wrong, producer tried to push into full "
@@ -95,7 +96,24 @@ int main(int argc, char *argv[]) {
     }
     char message_string[83 + TIMESTAMP_LENGTH + 1];
     message_tostring(&message, message_string);
-    log_info("Pushed %s into circular buffer", message_string);
+    log_info("Pushed %s into ["
+             "\x1b[1m"
+             "%zu"
+             "\x1b[22m"
+             "] of the circular buffer",
+             message_string, field);
+    log_info("    "
+             "There are "
+             "\x1b[1m"
+             "%ld"
+             "\x1b[22m"
+             " active consumers and "
+             "\x1b[1m"
+             "%ld"
+             "\x1b[22m"
+             " active producers",
+             atomic_integer_get(&shared_memory->active_consumer_counter),
+             atomic_integer_get(&shared_memory->active_producer_counter));
 
     sem_post(&shared_memory->full);
 
