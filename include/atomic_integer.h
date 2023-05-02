2a767c10e0f7b30f872d6230fc1e93991295da34
datwaft <datwaftx@gmail.com>
2023-05-01 22:45:37 -0600

    feat: add atomic_integer.h library

---
+++ include/atomic_integer.h
@@ -0,0 +1,20 @@
+#ifndef ATOMIC_INTEGER_H
+#define ATOMIC_INTEGER_H
+
+#include <semaphore.h>
+#include <stdint.h>
+
+typedef struct atomic_integer_st {
+  sem_t mutex;
+  int64_t value;
+} atomic_integer_t;
+
+atomic_integer_t atomic_integer_new(int64_t value);
+
+int64_t atomic_integer_get(atomic_integer_t *atomic_integer);
+
+int64_t atomic_integer_add(atomic_integer_t *atomic_integer, int64_t value);
+
+int64_t atomic_integer_sub(atomic_integer_t *atomic_integer, int64_t value);
+
+#endif // !ATOMIC_INTEGER_H
---
+++ src/atomic_integer.c
@@ -0,0 +1,32 @@
+#include "atomic_integer.h"
+
+#include <stdbool.h>
+
+atomic_integer_t atomic_integer_new(int64_t value) {
+  atomic_integer_t object = {.value = value};
+  sem_init(&object.mutex, true, 1);
+  return object;
+}
+
+int64_t atomic_integer_get(atomic_integer_t *atomic_integer) {
+  sem_wait(&atomic_integer->mutex);
+  int64_t result = atomic_integer->value;
+  sem_post(&atomic_integer->mutex);
+  return result;
+}
+
+int64_t atomic_integer_add(atomic_integer_t *atomic_integer, int64_t value) {
+  sem_wait(&atomic_integer->mutex);
+  atomic_integer->value += value;
+  int64_t result = atomic_integer->value;
+  sem_post(&atomic_integer->mutex);
+  return result;
+}
+
+int64_t atomic_integer_sub(atomic_integer_t *atomic_integer, int64_t value) {
+  sem_wait(&atomic_integer->mutex);
+  atomic_integer->value -= value;
+  int64_t result = atomic_integer->value;
+  sem_post(&atomic_integer->mutex);
+  return result;
+}
