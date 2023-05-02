0f82c4ee853ffafa9b91306d6368d8adf4e92799
datwaft <datwaftx@gmail.com>
2023-05-02 00:06:27 -0600

    feat: implement more random functions

--- include/random.h
+++ include/random.h
@@ -5,4 +5,8 @@
 
 int64_t rand_in_range(int64_t lower_bound, int64_t upper_bound);
 
+double rand_double(void);
+
+double rand_exp(double lambda);
+
 #endif // !RANDOM_H
--- src/random.c
+++ src/random.c
@@ -1,7 +1,16 @@
 #include "random.h"
 
+#include <math.h>
 #include <stdlib.h>
 
 int64_t rand_in_range(int64_t lower_bound, int64_t upper_bound) {
   return ((rand() % (upper_bound - lower_bound + 1)) + lower_bound);
+}
+
+double rand_double(void) { return rand() / (RAND_MAX + 1.0); }
+
+double rand_exp(double lambda) {
+  // ln(1 - u)/-λ where u is an uniform random number in [0,1[ range.
+  double u = rand_double();
+  return log(1 - u) / -lambda;
 }
