e0555f2d5f5a3cab8dd5d0d0c74d381439d44304
datwaft <datwaftx@gmail.com>
2023-05-02 14:44:29 -0600

    feat(consumer): better message consumption log

--- src/consumer.c
+++ src/consumer.c
@@ -70,6 +70,7 @@ int main(int argc, char *argv[]) {
     }
     sem_wait(&shared_memory->full);
 
+    size_t field = shared_memory->circbuf.tail;
     message_t message = circbuf_atomic_pop(&shared_memory->circbuf);
     if (!message_is_valid(&message)) {
       log_warn("Something went wrong, consumer tried to consume empty circular "
@@ -77,7 +78,24 @@ int main(int argc, char *argv[]) {
     }
     char message_string[83 + TIMESTAMP_LENGTH + 1];
     message_tostring(&message, message_string);
-    log_info("Consumed %s from circular buffer", message_string);
+    log_info("Consumed %s from ["
+             "\x1b[1m"
+             "%zu"
+             "\x1b[22m"
+             "] of the circular buffer",
+             message_string, field);
+    log_info("    "
+             "There are "
+             "\x1b[1m"
+             "%ld"
+             "\x1b[22m"
+             " active consumers and "
+             "\x1b[1m"
+             "%ld"
+             "\x1b[22m"
+             " active producers",
+             atomic_integer_get(&shared_memory->active_consumer_counter),
+             atomic_integer_get(&shared_memory->active_producer_counter));
 
     sem_post(&shared_memory->empty);
 
