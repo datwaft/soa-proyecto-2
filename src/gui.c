652573888c737d36daf3f306460816fde5720736
Michael <michaelyipchenmz@gmail.com>
2023-05-07 18:54:35 -0600

    feat: gui initial print of buffer content

--- src/gui.c
+++ src/gui.c
@@ -1,5 +1,6 @@
 #include "gui.h"
 
+#include "circbuf.h"
 #include "logging.h"
 #include "shared_memory.h"
 #include <math.h>
@@ -10,6 +11,9 @@
 #include <unistd.h>
 
 char *buffer_name_g;
+
+void print_all_buffer_contents(GtkTextBuffer *buffer,
+                               shared_mem_t *shared_memory);
 
 int launch_gui_log(char *buffer_name) {
   buffer_name_g = buffer_name;
@@ -32,9 +36,18 @@ void application_on_activate(GtkApplication *app, gpointer _) {
 
   GtkTextBuffer *buffer = GTK_TEXT_BUFFER(
       gtk_builder_get_object(user_data.builder, "txt_buff_circular_buffer"));
-  GtkTextIter end;
-  gtk_text_buffer_get_end_iter(buffer, &end);
-  gtk_text_buffer_insert(buffer, &end, "hello\n", -1);
+  // GtkTextIter end;
+  // gtk_text_buffer_get_end_iter(buffer, &end);
+
+  // ///
+
+  // for (int i = 0; i < 50; i++) {
+  //   char str[20];
+  //   sprintf(str, "%d  %s", i, "hello\n");
+  //   gtk_text_buffer_insert(buffer, &end, str, -1);
+  // }
+
+  ///
 
   GtkLabel *lbl_buffer_name =
       GTK_LABEL(gtk_builder_get_object(user_data.builder, "lbl_buffer_name"));
@@ -54,6 +67,7 @@ void application_on_activate(GtkApplication *app, gpointer _) {
 
   char consumer_counter_str[20];
   char producer_counter_str[20];
+  // char msg_id[53];
   shared_mem_t *shared_memory = get_shared_memory(buffer_name_g);
   while (1) {
     while (gtk_events_pending())
@@ -67,7 +81,41 @@ void application_on_activate(GtkApplication *app, gpointer _) {
 
     gtk_label_set_text(lbl_num_consumers, consumer_counter_str);
     gtk_label_set_text(lbl_num_producers, producer_counter_str);
+
+    print_all_buffer_contents(buffer, shared_memory);
   }
+}
+
+void print_all_buffer_contents(GtkTextBuffer *buffer,
+                               shared_mem_t *shared_memory) {
+
+  circbuf_t *circ_buf = &shared_memory->circbuf;
+  size_t size = circ_buf->max_size;
+  char msg_id[53];
+
+  GtkTextIter end;
+  gtk_text_buffer_get_end_iter(buffer, &end);
+
+  for (size_t i = 0; i < size; i++) {
+    message_t msg = circ_buf->array[i];
+
+    if (msg.producer_id > 0) {
+      sprintf(msg_id,
+              "Buffer Position: %ld | Producer Id: %ld | Random Key: %d\n", i,
+              msg.producer_id, msg.random_key);
+      // g_print(msg_id);
+      gtk_text_buffer_insert(buffer, &end, msg_id, -1);
+    } else {
+      sprintf(msg_id,
+              "Buffer Position: %ld | Producer Id: %s | Random Key: %s\n", i,
+              "-", "-");
+      // g_print(msg_id);
+      gtk_text_buffer_insert(buffer, &end, msg_id, -1);
+    }
+  }
+  // g_print("------------------------------------------\n");
+  gtk_text_buffer_insert(buffer, &end,
+                         "------------------------------------------\n", -1);
 }
 
 void window_on_delete_event(GtkWidget *widget, gpointer user_data) {
